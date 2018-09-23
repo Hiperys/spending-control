03e7aaffb33c6ec2da2673eb8441c1bde9764aba
Carlitosry <hola@carlitosry.com>
2018-09-23 17:50:35 +0200

    AuthService with Firebase Google Authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AppComponent } from './app.component';
-import {environment} from '../environments/environment';
+import { environment } from '../environments/environment';
 import { TransactionComponent } from './transaction/transaction.component';
-import {TransactionService} from './services/transaction.service';
+import { TransactionService } from './services/transaction.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { LoginComponent } from './login/login.component';
+import { AuthService } from './services/authentication/auth.service';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import {MatButtonModule, MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -19,11 +22,15 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
     MatCheckboxModule,
+    MatButtonModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   providers: [
+    AuthService,
     TransactionService
   ],
   bootstrap: [AppComponent]
---
+++ src/app/services/authentication/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private afAuth: AngularFireAuth) {  }
+
+  login () {
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  }
+
+  logout () {
+    this.afAuth.auth.signOut();
+  }
+}
